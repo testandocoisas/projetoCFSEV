313dd2e064e10276e6be3347b7deb51da35608b8
Italo <italog09@gmail.com>
2021-01-22 19:33:17 -0300

    versão 0.1

---
+++ main.c
@@ -0,0 +1,33 @@
+//////////////////////////////////////////////////////////////////////
+///         Universidade Federal do Rio Grande do Norte            ///  
+///            Centro de Ensino Superior do Seridó                 ///   
+///            Bacharelado em Sistema de Informação                ///   
+///              Disciplina DCT1106 -- Programação                 ///   
+///   Projeto Controle de fluxo para entrada e saida de veiculos   ///   
+///         Developed by  Ítalo G and Lucas V - Jan, 2021          ///   
+//////////////////////////////////////////////////////////////////////
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+void menuSobre(void);
+
+int main(void) {
+    menuSobre();
+    return 0;
+}
+
+
+void menuSobre(void) {
+    printf("         ////////////////////////////////////////////////////////////////////////////\n");
+    printf("        ///                 ///                                                  ///\n");
+    printf("       ///      *BSI*      ///                                                  ///\n");
+    printf("      ///                 ///                                                  ///\n");
+    printf("     ///////////////////////                                                  ///\n");
+    printf("    ///                                                                      ///\n");
+    printf("   ///                                                                      ///\n");
+    printf("  ///                                                                      ///\n");
+    printf(" ///                                                                      ///\n");
+    printf("////////////////////////////////////////////////////////////////////////////\n");
+}
